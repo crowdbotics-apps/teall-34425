608d0d1498f669472dfe42e41cf5f73023a149ee
Crowdbotics <team@crowdbotics.com>
2022-03-25 02:45:23 +0000

    Initial commit.

---
+++ backend/teall_34425/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for teall_34425 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teall_34425.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "teall_34425",
+  url: "https://teall_34425.botics.co",
+  api: "https://teall_34425.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
